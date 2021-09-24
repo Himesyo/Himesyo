61502a22745c5af6ec38e0f2a22ff7c66d054928
Himesyo <183147241@qq.com>
2021-09-24 18:15:53 +0800

    文件帮助类和对话框

---
+++ Himesyo/Himesyo/IO/FileHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Himesyo.IO
+{
+    /// <summary>
+    /// 和文件相关的辅助方法。
+    /// </summary>
+    public static class FileHelper
+    {
+        /// <summary>
+        /// 在系统资源管理器中显示。
+        /// </summary>
+        /// <param name="path"></param>
+        public static void ShowInExplorer(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                return;
+
+            if (File.Exists(path) || Directory.Exists(path))
+            {
+                Process.Start("explorer.exe", $"/select,{path}");
+                return;
+            }
+            string dir = Path.GetDirectoryName(path);
+            if (Directory.Exists(dir))
+            {
+                Process.Start("explorer.exe", dir);
+            }
+        }
+    }
+}
---
+++ Himesyo/Himesyo/WinForm/MsgBox.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Himesyo.WinForm
+{
+    [DebuggerDisplay("{Result} [Buttons = {Buttons}, Message = {Message}]")]
+    public class MsgBox
+    {
+        public static MsgBox Show(string msg, string title = "消息", MessageBoxButtons btns = MessageBoxButtons.OK, MessageBoxIcon icon = MessageBoxIcon.Information, IWin32Window owner = null)
+        {
+            MsgBox box = new MsgBox()
+            {
+                Message = msg,
+                Title = title,
+                Buttons = btns,
+                Icon = icon,
+                Owner = owner
+            };
+            box.Show();
+            return box;
+        }
+
+        public DialogResult Result { get; set; }
+        public IWin32Window Owner { get; set; }
+        public string Message { get; set; }
+        public string Title { get; set; } = "消息";
+        public MessageBoxButtons Buttons { get; set; } = MessageBoxButtons.OK;
+        public MessageBoxIcon Icon { get; set; } = MessageBoxIcon.Information;
+
+        public void Show()
+        {
+            if (Owner is Control control && control.InvokeRequired)
+            {
+                control.Invoke((MethodInvoker)Show);
+            }
+            else
+            {
+                Result = MessageBox.Show(Owner, Message, Title, Buttons, Icon);
+            }
+        }
+
+        public void Show(string newMsg)
+        {
+            Message = newMsg;
+            Show();
+        }
+
+        public void Show(string newMsg, MessageBoxButtons btns)
+        {
+            Message = newMsg;
+            Buttons = btns;
+            Show();
+        }
+    }
+}
